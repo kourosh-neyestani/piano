992aef32c28001ff0c3f6621a4c8e90dde0e6112
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-04-05 12:33:45 +0430

    add piano-keys

--- src/scenes/Piano.js
+++ src/scenes/Piano.js
@@ -4,6 +4,48 @@ import './index.scss';
 const Piano = () => {
     return (
         <div className="piano">
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
+            <div className="piano-key">
+                <div className="piano-key-white" />
+            </div>
         </div>
     )
 }
